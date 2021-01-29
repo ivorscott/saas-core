62178765b1b89000fc1292deaf70316d1d13129d
Ivor Scott <ivor@devpie.io>
2021-01-29 19:28:41 +0100

    refactor: updated Auth0 roles and rules

--- __auth0__/rules/set-claim--assigned-role.js
+++ __auth0__/rules/set-claim--assigned-role.js
@@ -1,14 +1,10 @@
 function SetAssignedRoles(user, context, callback) {
     const namespace = 'https://client.devpie.io/claims/roles';
 
-    let idTokenClaims = context.idToken || {};
-
     const assignedRoles = (context.authorization || {}).roles;
-    const defaultRoles = idTokenClaims[namespace] ? idTokenClaims[namespace] : [];
-    const combinedRoles = [...assignedRoles, ...defaultRoles];
 
-    context.idToken[namespace] = combinedRoles;
-    context.accessToken[namespace] = combinedRoles;
+    context.idToken[namespace] = assignedRoles;
+    context.accessToken[namespace] = assignedRoles;
 
     callback(null, user, context);
 }
