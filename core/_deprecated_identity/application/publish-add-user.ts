8e63b02cffa8dfcc5c4251deb8b2ef665678c9fb
Ivor Scott <ivor@devpie.io>
2021-04-28 04:28:33 +0200

    refactor: deprecate identity event sourcing module

--- core/_deprecated_identity/application/publish-add-user.ts
+++
@@ -1,5 +0,0 @@
-import { Commands, Publisher, AddUserCommand } from "@devpie/client-events";
-
- export class AddUserPublisher extends Publisher<AddUserCommand> {
-  readonly type = Commands.AddUser;
-}
--- core/_deprecated_identity/handler/publish-user-added.ts
+++
@@ -1,5 +0,0 @@
-import { Events, Publisher, UserAddedEvent } from "@devpie/client-events";
-
-export class UserAddedPublisher extends Publisher<UserAddedEvent> {
-  readonly type = Events.UserAdded;
-}
