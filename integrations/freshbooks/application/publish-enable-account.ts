2bbe3561fad9f760ebd159c803b67812b049a6b3
Ivor Scott Cummings <ivor@devpie.io>
2021-09-21 00:40:25 +0200

    ISSUE-53 remove CQRS experiment

--- integrations/freshbooks/application/publish-enable-account.ts
+++
@@ -1,5 +0,0 @@
-import { Commands, Publisher, EnableAccountingCommand } from "@devpie/client-events";
-
- export class EnableAccountingPublisher extends Publisher<EnableAccountingCommand> {
-  readonly type = Commands.EnableAccounting;
-}
