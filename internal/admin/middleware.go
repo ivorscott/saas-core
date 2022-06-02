09f5e173b10003d1fb756223a090416e8c25385e
Ivor Scott <ivor@devpie.io>
2022-06-02 23:01:39 +0200

    fix: redirect to / on missing session token

--- internal/admin/middleware.go
+++ internal/admin/middleware.go
@@ -11,7 +11,7 @@ func loadSession(next http.Handler) http.Handler {
 func withSession(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !session.Exists(r.Context(), "userID") {
-			http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		}
 		next.ServeHTTP(w, r)
 	})
