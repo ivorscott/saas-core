308cf7414b5746cc5e9dbfca52433177b31dfa32
Ivor Scott <ivor@devpie.io>
2022-06-08 01:23:03 +0200

    fix: add new Authenticate method to share auth middleware

--- pkg/web/mid/auth.go
+++ pkg/web/mid/auth.go
@@ -1,22 +1,18 @@
 package mid
 
 import (
-	"fmt"
-	"github.com/devpies/saas-core/pkg/web"
-	"github.com/lestrrat-go/jwx/jwk"
-	"github.com/lestrrat-go/jwx/jwt"
-	"go.uber.org/zap"
 	"net/http"
-	"strings"
+
+	"github.com/devpies/saas-core/pkg/web"
+
+	"go.uber.org/zap"
 )
 
 // Auth middleware verifies the id_token.
 func Auth(log *zap.Logger, region string, userPoolClientID string) web.Middleware {
-	// this is the actual middleware function to be executed.
 	f := func(after web.Handler) web.Handler {
-		// create the handler that will be attached in the middleware chain.
 		h := func(w http.ResponseWriter, r *http.Request) error {
-			err := verifyToken(w, r, region, userPoolClientID)
+			r, err := web.Authenticate(log, r, region, userPoolClientID)
 			if err != nil {
 				log.Info("api authentication failed", zap.Error(err))
 				return web.NewRequestError(err, http.StatusUnauthorized)
@@ -26,30 +22,4 @@ func Auth(log *zap.Logger, region string, userPoolClientID string) web.Middlewar
 		return h
 	}
 	return f
-}
-
-func verifyToken(w http.ResponseWriter, r *http.Request, region string, userPoolClientID string) error {
-	authHeader := r.Header.Get("Authorization")
-	splitAuthHeader := strings.Split(authHeader, " ")
-
-	if len(splitAuthHeader) != 2 {
-		return fmt.Errorf("missing or invalid authorization header")
-	}
-
-	pubKeyURL := "https://cognito-idp.%s.amazonaws.com/%s/.well-known/jwks.json"
-	formattedURL := fmt.Sprintf(pubKeyURL, region, userPoolClientID)
-
-	keySet, err := jwk.Fetch(r.Context(), formattedURL)
-	if err != nil {
-		return err
-	}
-
-	_, err = jwt.Parse(
-		[]byte(splitAuthHeader[1]),
-		jwt.WithKeySet(keySet),
-		jwt.WithValidate(true),
-	)
-
-	// Add user object to context
-	return err
 }
