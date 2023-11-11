ca008c1dd39d3b27371d51ecb02709738c50a33c
Ivor Scott <ivor@devpie.io>
2023-11-11 08:31:33 +0100

    fix: use correct db field in struct tag to retrieve database column value

--- internal/subscription/model/customer.go
+++ internal/subscription/model/customer.go
@@ -34,9 +34,9 @@ type Customer struct {
 	FirstName       string    `json:"firstName" db:"first_name"`
 	LastName        string    `json:"lastName" db:"last_name"`
 	Email           string    `json:"email" db:"email"`
-	PaymentMethodID string    `json:"paymentMethodId" db:"paymentMethod_id"`
-	UpdatedAt       time.Time `db:"updated_at" json:"updatedAt"`
-	CreatedAt       time.Time `db:"created_at" json:"createdAt"`
+	PaymentMethodID string    `json:"paymentMethodId" db:"payment_method"`
+	UpdatedAt       time.Time `json:"updatedAt" db:"updated_at"`
+	CreatedAt       time.Time `json:"createdAt" db:"created_at"`
 }
 
 // UpdateCustomer represents a customer update.
