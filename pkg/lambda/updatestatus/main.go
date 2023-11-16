721f8999125c0ab57c5be2de4431416cb9584182
Ivor Scott <ivor@devpie.io>
2023-11-16 09:59:59 +0100

    fix: dynamodb failure while using reserved words

--- pkg/lambda/updatestatus/main.go
+++ pkg/lambda/updatestatus/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -11,28 +12,33 @@ import (
 )
 
 func handler(ctx context.Context, event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error) {
-	var err error
-
 	client := repository.NewDynamoRepository(ctx, event.Region)
 
 	id, ok := event.Request.UserAttributes["custom:tenant-id"]
 	if !ok {
-		return event, nil
+		return event, fmt.Errorf("error: missing tenant-id")
 	}
 
-	_, err = client.UpdateItem(context.TODO(), &dynamodb.UpdateItemInput{
+	fmt.Printf("%s", id)
+
+	out, err := client.UpdateItem(ctx, &dynamodb.UpdateItemInput{
 		TableName: aws.String("local-tenants"),
 		Key: map[string]types.AttributeValue{
 			"tenantId": &types.AttributeValueMemberS{Value: id},
 		},
-		UpdateExpression: aws.String("set status = :status"),
+		UpdateExpression: aws.String("set #S = :status"),
+		ExpressionAttributeNames: map[string]string{
+			"#S": "status",
+		},
 		ExpressionAttributeValues: map[string]types.AttributeValue{
 			":status": &types.AttributeValueMemberS{Value: "confirmed"},
 		},
+		ReturnValues: types.ReturnValueUpdatedNew,
 	})
 	if err != nil {
-		return event, nil
+		return event, fmt.Errorf("error: %w", err)
 	}
+	fmt.Printf("%v", out.Attributes)
 
 	return event, nil
 }
